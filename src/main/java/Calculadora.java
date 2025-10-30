0979fddfaa169a4ced64385ee6134100bb2c4eeb
BCZLopezCatunta <bl2020068946@virtual.upt.pe>
2025-10-29 21:27:03 -0500

    Subido Docs

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+    
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    
+    public int restar(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+    
+    public double dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
+    
+    public boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+}
